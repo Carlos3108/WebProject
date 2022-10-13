77c1b6d86a7e81c650eebb56d912a985b46a379f
csilv <c.silva3108@hotmail.com>
2022-10-13 02:38:56 -0300

    ""

--- src/main/java/com/azdevelopment/webproject/repository/UsuarioRepository.java
+++ src/main/java/com/azdevelopment/webproject/repository/UsuarioRepository.java
@@ -2,7 +2,12 @@ package com.azdevelopment.webproject.repository;
 
 import com.azdevelopment.webproject.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface UsuarioRepository extends MongoRepository <Usuario, String>{
-
+    @Query
+    public List<Usuario> obterUsuarioPorNascimento(Date de, Date ate);
 }
