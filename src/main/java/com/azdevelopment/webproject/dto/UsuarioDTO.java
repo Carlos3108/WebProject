84d26436978551b649b8c8b35e3b7f745fbc6aca
Carlos3108 <c.silva3108@hotmail.com>
2022-12-08 15:07:15 -0300

    Definido estrutura de dados DTO.

---
+++ src/main/java/com/azdevelopment/webproject/dto/UsuarioDTO.java
@@ -0,0 +1,21 @@
+package com.azdevelopment.webproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+    private String id;
+    private String nome;
+    private Date nascimento;
+    private String email;
+
+}
