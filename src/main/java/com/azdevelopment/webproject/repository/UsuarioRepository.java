c6d7842c06baa72994283ff003e66cf204b1e72b
Carlos3108 <c.silva3108@hotmail.com>
2022-12-13 11:45:13 -0300

    Adicionado a anotação @Repository na class UsuarioRepository

--- src/main/java/com/azdevelopment/webproject/repository/UsuarioRepository.java
+++ src/main/java/com/azdevelopment/webproject/repository/UsuarioRepository.java
@@ -2,8 +2,9 @@ package com.azdevelopment.webproject.repository;
 
 import com.azdevelopment.webproject.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
 
 }
