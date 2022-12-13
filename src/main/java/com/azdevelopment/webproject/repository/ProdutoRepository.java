eb739e1a38629900f82114e5398a230fad2ef4e2
Carlos3108 <c.silva3108@hotmail.com>
2022-12-13 12:04:46 -0300

    Otimizacao de imports e formatacao

--- src/main/java/com/azdevelopment/webproject/repository/ProdutoRepository.java
+++ src/main/java/com/azdevelopment/webproject/repository/ProdutoRepository.java
@@ -1,6 +1,5 @@
 package com.azdevelopment.webproject.repository;
 
-import com.azdevelopment.webproject.dto.ProdutoDTO;
 import com.azdevelopment.webproject.model.Produto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/azdevelopment/webproject/service/ProdutoService.java
+++ src/main/java/com/azdevelopment/webproject/service/ProdutoService.java
@@ -1,7 +1,6 @@
 package com.azdevelopment.webproject.service;
 
 import com.azdevelopment.webproject.dto.ProdutoDTO;
-import com.azdevelopment.webproject.dto.UsuarioDTO;
 
 public interface ProdutoService {
     ProdutoDTO criar(ProdutoDTO produto);
