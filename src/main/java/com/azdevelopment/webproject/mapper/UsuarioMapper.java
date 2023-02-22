1faced852f58edb6c1e18d51e15782c3f73b4c0f
Carlos3108 <c.silva3108@hotmail.com>
2023-02-22 20:10:16 -0300

    Definido metodo com as devidas validacoes para Update usuario

--- src/main/java/com/azdevelopment/webproject/mapper/UsuarioMapper.java
+++ src/main/java/com/azdevelopment/webproject/mapper/UsuarioMapper.java
@@ -1,10 +1,14 @@
 package com.azdevelopment.webproject.mapper;
 
 import com.azdevelopment.webproject.dto.UsuarioDTO;
+import com.azdevelopment.webproject.exception.WebProjectError;
+import com.azdevelopment.webproject.exception.WebProjectException;
 import com.azdevelopment.webproject.model.Usuario;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -30,6 +34,32 @@ public class UsuarioMapper {
                 .email(usuario.getEmail())
                 .nascimento(usuario.getNascimento())
                 .build();
+    }
+
+    public UsuarioDTO fromUpdate(UsuarioDTO usuarioDTO, Usuario usuario) {
+
+        return UsuarioDTO.builder()
+                .id(usuario.getId())
+                .nome(isDiferent(usuarioDTO.getNome(), usuario.getNome()))
+                .email(isDiferent(usuarioDTO.getEmail(), usuario.getEmail()))
+                .nascimento(new Date(isDiferent(usuarioDTO.getNascimento().toString(),
+                        usuario.getNascimento().toString())))
+                .senha(isDiferent(usuarioDTO.getSenha(), usuario.getSenha()))
+                .build();
+    }
+
+    private static String isDiferent(String newValue, String oldValue) {
+        if (newValue != null && !newValue.equals(oldValue))
+            return isValid(newValue);
+        else
+            return isValid(oldValue);
+    }
+
+    @SneakyThrows
+    private static String isValid(String value) {
+        if (value.trim().isEmpty())
+            throw new WebProjectException(WebProjectError.VALUE_IS_INVALID);
+        return value;
     }
 
     public Usuario to(UsuarioDTO usuario) {
