ed9f5b21793b45f41ab6b2ea99fd50a57ba97156
Carlos3108 <c.silva3108@hotmail.com>
2023-04-05 21:58:56 -0300

    Romovido o endpoit de servico

--- src/main/java/com/azdevelopment/webproject/service/ProdutoService.java
+++
@@ -1,7 +0,0 @@
-package com.azdevelopment.webproject.service;
-
-import com.azdevelopment.webproject.dto.ProdutoDTO;
-
-public interface ProdutoService {
-    ProdutoDTO criar(ProdutoDTO produto);
-}
