6d1d92f9ed4977e3d448617abcbf617e30995ac2
Carlos3108 <c.silva3108@hotmail.com>
2023-04-12 16:39:59 -0300

    otimizado imports

--- src/main/java/com/azdevelopment/webproject/controller/UserController.java
+++ src/main/java/com/azdevelopment/webproject/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/user")
